a6cdf348db04fe2a363490a3a750bf9a97992efe
Shivansh Joshi <77457296+sniperzee2@users.noreply.github.com>
2022-05-13 01:14:31 +0530

    make it better

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -202,7 +202,7 @@ exports.mergeVideos = async (req, res) => {
     let array = []
     for (var i = 0; i < video_file_path_list.length; i++) {
         array.push({
-            fileName: `${path.join(__dirname, '..', `${video_file_path_list[i]}`)}`
+            fileName: `${path.join(__dirname, '../..', `${video_file_path_list[i]}`)}`
         })
     }
 
